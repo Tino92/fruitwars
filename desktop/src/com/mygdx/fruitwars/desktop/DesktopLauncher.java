a26d9da91aacecfbcb0549b939193ba8194d074f
Tino92 <tinolazreg@gmail.com>
2015-03-20 11:39:15 +0100

    project setup

---
+++ desktop/src/com/mygdx/fruitwars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.fruitwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.fruitwars.FruitWarsMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FruitWarsMain(), config);
+	}
+}
