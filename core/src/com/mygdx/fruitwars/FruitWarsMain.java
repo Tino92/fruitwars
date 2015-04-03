3afeac251d94e50ef3539daf8b21f4485d721575
kwrl <haakongk@stud.ntnu.no>
2015-04-03 17:35:05 +0200

    Tiled map

--- core/src/com/mygdx/fruitwars/FruitWarsMain.java
+++ core/src/com/mygdx/fruitwars/FruitWarsMain.java
@@ -1,27 +1,112 @@
 package com.mygdx.fruitwars;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class FruitWarsMain extends Game{
-	SpriteBatch batch;
-	Texture img;
-	//Comment
+public class FruitWarsMain extends Game implements InputProcessor {
+	private Texture img;
+	private TiledMap map;
+	private OrthographicCamera camera;
+	private TiledMapRenderer renderer;
+	private World world;
+	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		float w, h;
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
+		camera.update();
+		
+		map = new TmxMapLoader().load("test.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
+		Gdx.input.setInputProcessor(this);
+		
+		world = new World(new Vector2(0.0f, 2.0f), true);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+            camera.translate(-32,0);
+        if(keycode == Input.Keys.RIGHT)
+            camera.translate(32,0);
+        if(keycode == Input.Keys.UP)
+            camera.translate(0,-32);
+        if(keycode == Input.Keys.DOWN)
+            camera.translate(0,32);
+        if(keycode == Input.Keys.NUM_1)
+            map.getLayers().get(0).setVisible(!map.getLayers().get(0).isVisible());
+        if(keycode == Input.Keys.NUM_2)
+            map.getLayers().get(1).setVisible(!map.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
