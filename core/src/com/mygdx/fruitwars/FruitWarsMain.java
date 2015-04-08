0e1e187fd9e435a124211c8decc224d7c59364e3
kwrl <haakongk@stud.ntnu.no>
2015-04-08 14:54:44 +0200

    Basic box2d test. Working as expected, shitty code

--- core/src/com/mygdx/fruitwars/FruitWarsMain.java
+++ core/src/com/mygdx/fruitwars/FruitWarsMain.java
@@ -1,21 +1,29 @@
 package com.mygdx.fruitwars;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.fruitwars.tokens.Ball;
 
 public class FruitWarsMain extends Game implements InputProcessor {
 	private Texture img;
@@ -23,9 +31,15 @@ public class FruitWarsMain extends Game implements InputProcessor {
 	private OrthographicCamera camera;
 	private TiledMapRenderer renderer;
 	private World world;
+	private Box2DDebugRenderer debugRenderer;
+	private Vector2 touchDown;
+	private Array<Body> bodies;
+	private SpriteBatch sb;
 	
 	@Override
 	public void create () {
+		bodies = new Array<Body>();
+		sb = new SpriteBatch();
 		float w, h;
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
@@ -38,18 +52,74 @@ public class FruitWarsMain extends Game implements InputProcessor {
 		renderer = new OrthogonalTiledMapRenderer(map);
 		Gdx.input.setInputProcessor(this);
 		
-		world = new World(new Vector2(0.0f, 2.0f), true);
+		world = new World(new Vector2(0.0f, -0.5f), true);
+		debugRenderer = new Box2DDebugRenderer();
+		int blockWidth = (int)camera.viewportWidth/10;
+		for(int i = 0; i < 10; i++) {
+			createBlock(new Vector2(blockWidth*i, 10), new Vector2(blockWidth, 10));
+		}
 	}
+	
+	
+	public Fixture createBall(Vector2 pos) {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(pos);
+		Body body = world.createBody(bodyDef);
+		CircleShape circle = new CircleShape();
+		circle.setRadius(6f);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 10f;
+		fixtureDef.friction = 0.1f;
+		fixtureDef.restitution = 0.9f;
+		Ball ball = new Ball();
+		ball.setSize(circle.getRadius()*2, circle.getRadius()*2);
+		body.setUserData(ball);
+		return body.createFixture(fixtureDef);
+	}
+	
+	public Fixture createGround() {
+		BodyDef groundBodyDef = new BodyDef();
+		groundBodyDef.position.set(new Vector2(0, 10));
+		Body groundBody = world.createBody(groundBodyDef);
+		PolygonShape groundBox = new PolygonShape();
+		groundBox.setAsBox(camera.viewportWidth, 10.0f);
+		return groundBody.createFixture(groundBox, 0.0f);
+	}
+	
+	public Fixture createBlock(Vector2 pos, Vector2 dim) {
+		BodyDef blockBodyDef = new BodyDef();
+		blockBodyDef.position.set(pos);
+		Body blockBody = world.createBody(blockBodyDef);
+		PolygonShape block = new PolygonShape();
+		block.setAsBox(dim.x, dim.y);
+		return blockBody.createFixture(block, 0.0f);
+	}
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		float dt = Math.max(Gdx.graphics.getDeltaTime(), 0.25f);
+        sb.begin();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        renderer.setView(camera);
-        renderer.render();
+        //camera.update();
+        //renderer.setView(camera);
+        //renderer.render();
+        for(Body b : bodies) {
+        	if(b.getUserData()!=null) {
+        		Ball ball = (Ball)b.getUserData();
+        		ball.setPosition(b.getPosition().x, b.getPosition().y);
+        		ball.draw(sb);
+        	}
+        }
+        sb.end();
+		//debugRenderer.render(world, camera.combined);
+		world.step(dt, 6, 2);
 	}
+	
 
 	@Override
 	public boolean keyDown(int keycode) {
@@ -82,19 +152,27 @@ public class FruitWarsMain extends Game implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		this.touchDown = new Vector2(screenX, camera.viewportHeight-screenY);
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		if(touchDown == null) {
+			return false;
+		}
+		Vector2 velocity = new Vector2(screenX, camera.viewportHeight-screenY).sub(touchDown).rotate(180).scl(100);
+		Fixture ball = createBall(touchDown);
+		ball.getBody().applyForce(velocity, ball.getBody().getPosition(), true);
+		touchDown = null;
+		bodies.add(ball.getBody());
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
+		Fixture ball = createBall(new Vector2(screenX, camera.viewportHeight-screenY));
+		bodies.add(ball.getBody());
 		return false;
 	}
 
--- core/src/com/mygdx/fruitwars/tokens/Block.java
+++ core/src/com/mygdx/fruitwars/tokens/Block.java
@@ -1,5 +1,5 @@
 package com.mygdx.fruitwars.tokens;
 
 public class Block {
-
+	
 }
