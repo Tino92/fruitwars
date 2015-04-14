333e86d9be3830fff428f15939e8d03573956fd8
kwrl <haakongk@stud.ntnu.no>
2015-04-14 11:35:51 +0200

    Map loader working with box2d

--- core/src/com/mygdx/fruitwars/FruitWarsMain.java
+++ core/src/com/mygdx/fruitwars/FruitWarsMain.java
@@ -9,7 +9,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -17,52 +18,91 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
-import com.mygdx.fruitwars.tokens.Ball;
-import com.mygdx.fruitwars.tokens.Block;
-import com.mygdx.fruitwars.tokens.SpriteFixture;
 
 public class FruitWarsMain extends Game implements InputProcessor {
+	private static float ppt = 0;
 	private Texture img;
 	private TiledMap map;
 	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
 	private World world;
 	private Box2DDebugRenderer debugRenderer;
 	private SpriteBatch sb;
-	
+
 	@Override
-	public void create () {
-		sb = new SpriteBatch();
+	public void create() {
 		float w, h;
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
-		
+
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, w, h);
 		camera.update();
-		
-		map = new TmxMapLoader().load("test.tmx");
+
+		map = new TmxMapLoader().load("maps/map.tmx");
 		Gdx.input.setInputProcessor(this);
-		
+
 		world = new World(new Vector2(0.0f, -0.5f), true);
 		debugRenderer = new Box2DDebugRenderer();
+		
+		renderer = new OrthogonalTiledMapRenderer(map);
+		
+		Array<Body> bodies = buildShapes(map, 1, world);
 	}
-	
 
 	@Override
-	public void render () {
+	public void render() {
 		float dt = Math.max(Gdx.graphics.getDeltaTime(), 0.25f);
-        sb.begin();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    }
-	
+		renderer.setView(camera);
+		renderer.render();
+		debugRenderer.render(world, camera.combined);
+	}
+
+	public Array<Body> buildShapes(TiledMap map, float pixels,
+			World world) {
+		ppt = pixels;
+		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
+		Array<Body> bodies = new Array<Body>();
+		
+		float cellWidth, cellHeight;
+
+		for (int x = 0; x < layer.getWidth(); x++) {
+			for (int y = 0; y < layer.getHeight(); y++) {
+				Cell cell = layer.getCell(x, y);
+				if(cell==null) {
+					continue;
+				}
+				cellWidth = cell.getTile().getTextureRegion().getRegionWidth();
+				cellHeight = cell.getTile().getTextureRegion().getRegionHeight();
+				Shape shape;
+				
+				PolygonShape polygon = new PolygonShape();
+		        Vector2 size = new Vector2((x*cellWidth + cellWidth* 0.5f) / ppt,
+		                                   (y*cellHeight + cellHeight * 0.5f ) / ppt);
+		        polygon.setAsBox(cellWidth * 0.5f / ppt,
+		                         cellHeight * 0.5f / ppt,
+		                         size,
+		                         0.0f);
+		        shape = polygon;
+				BodyDef bd = new BodyDef();
+				bd.type = BodyType.StaticBody;
+				Body body = world.createBody(bd);
+				body.createFixture(shape, 1);
+
+				bodies.add(body);
+
+				shape.dispose();
+			}
+		}
+		return bodies;
+	}
 
 	@Override
 	public boolean keyDown(int keycode) {
@@ -72,18 +112,20 @@ public class FruitWarsMain extends Game implements InputProcessor {
 
 	@Override
 	public boolean keyUp(int keycode) {
-		if(keycode == Input.Keys.LEFT)
-            camera.translate(-32,0);
-        if(keycode == Input.Keys.RIGHT)
-            camera.translate(32,0);
-        if(keycode == Input.Keys.UP)
-            camera.translate(0,-32);
-        if(keycode == Input.Keys.DOWN)
-            camera.translate(0,32);
-        if(keycode == Input.Keys.NUM_1)
-            map.getLayers().get(0).setVisible(!map.getLayers().get(0).isVisible());
-        if(keycode == Input.Keys.NUM_2)
-            map.getLayers().get(1).setVisible(!map.getLayers().get(1).isVisible());
+		if (keycode == Input.Keys.LEFT)
+			camera.translate(-32, 0);
+		if (keycode == Input.Keys.RIGHT)
+			camera.translate(32, 0);
+		if (keycode == Input.Keys.UP)
+			camera.translate(0, -32);
+		if (keycode == Input.Keys.DOWN)
+			camera.translate(0, 32);
+		if (keycode == Input.Keys.NUM_1)
+			map.getLayers().get(0)
+					.setVisible(!map.getLayers().get(0).isVisible());
+		if (keycode == Input.Keys.NUM_2)
+			map.getLayers().get(1)
+					.setVisible(!map.getLayers().get(1).isVisible());
 		return false;
 	}
 
@@ -95,29 +137,29 @@ public class FruitWarsMain extends Game implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		//this.touchDown = new Vector2(screenX, camera.viewportHeight-screenY);
+		// this.touchDown = new Vector2(screenX, camera.viewportHeight-screenY);
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		/*if(touchDown == null) {
-			return false;
-		}
-		Vector2 velocity = new Vector2(screenX, camera.viewportHeight-screenY).sub(touchDown).rotate(180).scl(100);
-		Fixture ball = createBall(touchDown);
-		ball.getBody().applyForce(velocity, ball.getBody().getPosition(), true);
-		touchDown = null;
-		bodies.add(ball.getBody());
-		*/
+		/*
+		 * if(touchDown == null) { return false; } Vector2 velocity = new
+		 * Vector2(screenX,
+		 * camera.viewportHeight-screenY).sub(touchDown).rotate(180).scl(100);
+		 * Fixture ball = createBall(touchDown);
+		 * ball.getBody().applyForce(velocity, ball.getBody().getPosition(),
+		 * true); touchDown = null; bodies.add(ball.getBody());
+		 */
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		/*Fixture ball = createBall(new Vector2(screenX, camera.viewportHeight-screenY));
-		bodies.add(ball.getBody());
-		*/
+		/*
+		 * Fixture ball = createBall(new Vector2(screenX,
+		 * camera.viewportHeight-screenY)); bodies.add(ball.getBody());
+		 */
 		return false;
 	}
 
